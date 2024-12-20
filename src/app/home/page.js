956093702292c29fcbf1c27d0367210437245979
Joseph Etim <josephetim211@gmail.com>
2024-12-20 06:56:07 +0100

    fixes

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -20,7 +20,7 @@ const navItems = [
 
 export function LandingPage() {
   return (
-    <div className="flex overflow-hidden flex-col  pt-5 bg-stone-950 max-md:pt-24">
+    <div className="flex overflow-hidden items-center flex-col  pt-5 bg-stone-950 max-md:pt-24">
       
       <NavBar />
       <div className="flex flex-col items-center">
