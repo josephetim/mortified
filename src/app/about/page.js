2ad5426201f5ba19b897c44ffc3828c52255b0dd
Joseph Etim <josephetim211@gmail.com>
2024-12-23 10:58:24 +0100

    fixes

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -12,7 +12,7 @@ import { Footer } from "../components/Footer";
 
 export default function About () {
     return (
-        <div className="bg-stone-950 pt-6 ">
+        <div className="bg-stone-950 pt-6 pl-[70px] pr-[87px]">
             <div className="flex items-center justify-center">
             <NavBar />
             </div>
@@ -25,7 +25,7 @@ export default function About () {
                     />
                 </div>
 
-                <div className="flex overflow-hidden flex-col justify-center items-center self-stretch px-16 py-32  w-full bg-stone-950 max-md:px-5 max-md:py-24 max-md:mt-10 max-md:max-w-full">
+                <div className="flex overflow-hidden flex-col justify-center items-center self-stretch py-32  w-full bg-stone-950 max-md:px-5 max-md:py-24 max-md:mt-10 max-md:max-w-full">
         <div className="flex flex-wrap gap-10 justify-between w-full items-center -mb-6 max-md:mb-2.5 max-md:max-w-full">
           <div className="flex flex-col self-stretch my-auto min-w-[240px] w-[426px] max-md:max-w-full">
             <div className="flex flex-col w-full font-semibold max-md:max-w-full">
@@ -54,14 +54,14 @@ export default function About () {
         </div>
                 </div>
 
-                <div className="flex overflow-hidden flex-col justify-center items-center self-stretch px-16 py-32  w-full bg-stone-950 max-md:px-5 max-md:py-24 max-md:mt-10 max-md:max-w-full">
+                <div className="flex overflow-hidden flex-col justify-center items-center self-stretch  py-32  w-full bg-stone-950 max-md:px-5 max-md:py-24 max-md:mt-10 max-md:max-w-full">
         <div className="flex flex-wrap gap-10 justify-between w-full items-center -mb-6 max-md:mb-2.5 max-md:max-w-full">
           <div className="flex flex-col self-stretch my-auto min-w-[240px] w-[426px] max-md:max-w-full">
             <div className="flex flex-col w-full font-semibold max-md:max-w-full">
               <div className="font-kumbhSans gap-2.5 self-start px-4 py-2.5 text-[15px] font-normal  text-white rounded-3xl bg-zinc-800">
                 OUR VISION
               </div>
-              <div className=" font-kumbhSans text-[36px] font-normal mt-6 text-4xl text-neutral-200 max-md:max-w-full">
+              <div className=" font-kumbhSans text-[36px] font-semibold mt-6 text-4xl text-neutral-200 max-md:max-w-full">
               To nurture <span className="text-[#828282]"> Spirit-led </span>  believers, fulfilling <span className="text-[#828282]"> purpose </span>, advancing <span className="text-[#828282]"> God's kingdom </span> , and <span className="text-[#828282]"> transforming </span>  lives globally
                 
               </div>
@@ -76,15 +76,15 @@ export default function About () {
           </div>
           <div className="flex flex-col self-stretch my-auto text-xl leading-8 min-w-[240px] w-[603px] max-md:max-w-full">
            
-            <div className="font-kumbhSans mt-6 text-stone-300 max-md:max-w-full">
+            <div className="font-kumbhSans mt-6 text-[#BDBDBD] max-md:max-w-full">
             Our greatest concern at Mortified is the believer’s growth and a walk patterned to that of the Spirit while stressing issues relating to purpose and the advancement of God’s kingdom. Our motivation is the earnest desire to see the growing believer loving God without recourse to the systems of this world and to be actively participatory in God’s agenda for this time and age.
             </div>
 
-            <div className="font-kumbhSans mt-6 text-stone-300 max-md:max-w-full">
+            <div className="font-kumbhSans mt-6 text-[#BDBDBD] max-md:max-w-full">
             We are dedicated to inspiring believers to actively participate in God’s agenda for this time and age. This involves equipping them with the understanding, resources, and encouragement to discern their purpose, embrace their calling, and impact their generation with the transformative power of the gospel.
             </div>
 
-            <div className="font-kumbhSans mt-6 text-stone-300 max-md:max-w-full">
+            <div className="font-kumbhSans mt-6 text-[#BDBDBD] max-md:max-w-full">
             In all that we do, Mortified seeks to create an environment where spiritual growth thrives, purpose is discovered, and a Christ-centred lifestyle becomes the defining hallmark of every believer's walk with God.
             </div>
 
@@ -92,33 +92,33 @@ export default function About () {
         </div>
                 </div>
 
-                <div className="flex overflow-hidden flex-col self-stretch px-12 py-20 w-full bg-stone-950 max-md:px-5 max-md:max-w-full">
+                <div className="flex overflow-hidden flex-col self-stretch  py-20 w-full bg-stone-950 max-md:px-5 max-md:max-w-full">
 
         <div className="flex flex-col justify-center self-center max-w-full font-semibold w-[728px]">
           <div className="flex flex-col justify-center w-full max-md:max-w-full">
             <div className="font-kumbhSans gap-2.5 self-center px-4 py-2.5 text-[15px] font-normal text-white rounded-3xl bg-zinc-800">
               THE PASTORATE
             </div>
-            <div className="font-kumbhSans font-normal text-[36px] mt-6 text-4xl text-center text-neutral-200 max-md:max-w-full">
+            <div className="font-kumbhSans font-semibold text-[36px] mt-6 text-4xl text-center text-neutral-200 max-md:max-w-full">
             The pastorate is the bedrock of the Mortified Platform and where decisions pertaining to the network is made.
             </div>
           </div>
         </div>
-        <div className="flex flex-row flex-wrap gap-16 justify-between items-center pt-[130px]">
+        <div className="flex flex-row flex-wrap gap-[30px] justify-between items-center pt-[130px]">
             {pastorate.map((pastor, index) => <PastorateCard key={index} icon={pastor.icon} name={pastor.name} title={pastor.title} />)}
         </div>
        
       </div>
 
 
-      <div className="flex overflow-hidden flex-col justify-center items-center self-stretch px-16 py-32 w-full bg-stone-950 max-md:px-5 max-md:py-24 max-md:mt-10 max-md:max-w-full">
+      <div className="flex overflow-hidden flex-col justify-center items-center self-stretch  py-32 w-full bg-stone-950 max-md:px-5 max-md:py-24 max-md:mt-10 max-md:max-w-full">
         <div className="flex flex-wrap justify-between w-full gap-10 items-center -mb-6 max-md:mb-2.5 max-md:max-w-full">
           <div className="flex flex-col self-stretch my-auto min-w-[240px] w-[426px] max-md:max-w-full">
             <div className="flex flex-col w-full font-semibold max-md:max-w-full">
               <div className="font-kumbhSans gap-2.5 self-start px-4 py-2.5 text-[15px] font-normal  text-white rounded-3xl bg-zinc-800">
                 OUR DEPARTMENTS
               </div>
-              <div className=" font-kumbhSans text-[36px] font-normal mt-6 text-4xl text-neutral-200 max-md:max-w-full">
+              <div className=" font-kumbhSans text-[36px] font-semibold mt-6 text-4xl text-neutral-200 max-md:max-w-full">
               We operate efficiently by that which every joint supplies.
               </div>
             </div>
@@ -149,14 +149,13 @@ export default function About () {
           </div>
         </div>
       </div>
-        <div className="px-4">
+        <div >
             <Accordion items={administration} />
         </div>
-        <div className="mt-[149px] px-[84px] mb-[160px]">
+        <div className="mt-[149px]   mb-[160px]">
             <PartnershipSection />
         </div>
             </div>
-
             <Footer />
         </div>
     )
