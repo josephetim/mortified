2cfb3b7dd264f77c1ab6a7aa4ff7aa7ec8ec8dc6
Joseph Etim <josephetim211@gmail.com>
2024-12-24 12:19:20 +0100

    fixes

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -22,6 +22,21 @@ export const Footer = () => {
               LOGO
             </div>
             <div className="mt-6 text-stone-500 font-kumbhSans">The Mortified One</div>
+            <div className="flex flex-wrap gap-10 justify-between items-center mt-20 max-w-full w-[990px] max-md:mt-10">
+          <div className="flex gap-1 items-center self-stretch my-auto text-xl min-w-[240px] mt-[92px] text-stone-500">
+            <div className="self-stretch my-auto w-[100px] font-normal font-kumbhSans">Copyright</div>
+            <Image
+              loading="lazy"
+              src={icons.copyright}
+              alt="Copyright symbol"
+              className="object-contain shrink-0 self-stretch my-auto aspect-square w-[25px]"
+            />
+            <div className="self-stretch font-normal my-auto w-[234px] font-kumbhSans">
+              The Mortified One 2024
+            </div>
+          </div>
+          
+        </div>
           </div>
           <div className="flex gap-10 items-start min-w-[240px]">
             <div className="flex flex-col w-[137px]">
@@ -31,6 +46,11 @@ export const Footer = () => {
                   <FooterLink key={index} text={link} />
                 ))}
               </div>
+              <div className="flex gap-6 items-center mt-[86.5px] self-stretch my-auto">
+            {socialIcons.map((icon, index) => (
+              <SocialIcon key={index} {...icon} />
+            ))}
+          </div>
             </div>
             <div className="flex flex-col whitespace-nowrap w-[196px]">
               <div className="font-semibold text-white font-kumbhSans">Website</div>
@@ -40,25 +60,7 @@ export const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-wrap gap-10 justify-between items-center mt-20 max-w-full w-[990px] max-md:mt-10">
-          <div className="flex gap-1 items-center self-stretch my-auto text-xl min-w-[240px] text-stone-500">
-            <div className="self-stretch my-auto w-[100px] font-kumbhSans">Copyright</div>
-            <Image
-              loading="lazy"
-              src={icons.copyright}
-              alt="Copyright symbol"
-              className="object-contain shrink-0 self-stretch my-auto aspect-square w-[25px]"
-            />
-            <div className="self-stretch my-auto w-[234px] font-kumbhSans">
-              The Mortified One 2024
-            </div>
-          </div>
-          <div className="flex gap-6 items-center self-stretch my-auto">
-            {socialIcons.map((icon, index) => (
-              <SocialIcon key={index} {...icon} />
-            ))}
-          </div>
-        </div>
+       
       </div>
     </div>
   );
