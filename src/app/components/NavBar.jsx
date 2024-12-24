a529bf593aee83e3fefb667245b9ae921f13f69b
Joseph Etim <josephetim211@gmail.com>
2024-12-24 12:26:04 +0100

    fixes

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,8 +1,7 @@
-"use client"
+"use client";
 import * as React from "react";
 import Image from "next/image";
 import Link from "next/link"; // For Next.js navigation
-import { Kumbh_Sans } from "next/font/google";
 import { icons } from "../assets/icons/icons";
 
 export function NavItem({ label, href, isActive, onClick }) {
@@ -14,23 +13,31 @@ export function NavItem({ label, href, isActive, onClick }) {
       } whitespace-nowrap cursor-pointer`}
       onClick={onClick}
     >
-
-      {label === "LOGO" && <Image
-        alt="logo"
-        className="aspect-square w-[45px] h-[45px] object-contain"
-        src={icons.logo}
-      />}
+      {label === "LOGO" && (
+        <Image
+          alt="logo"
+          className="aspect-square w-[45px] h-[45px] object-contain"
+          src={icons.logo}
+        />
+      )}
       {label !== "LOGO" && label}
     </Link>
   );
 }
 
 export function NavBar() {
-  const pageNumber = localStorage.getItem("page")
-  const [activeIndex, setActiveIndex] = React.useState(+pageNumber); // Default to "Home"
+  const [activeIndex, setActiveIndex] = React.useState(0); // Default to "Home"
+
+  React.useEffect(() => {
+    // Only run this code on the client
+    const pageNumber = localStorage.getItem("page");
+    if (pageNumber) {
+      setActiveIndex(+pageNumber);
+    }
+  }, []); // Empty dependency array ensures this runs only once on mount
 
   const navItems = [
-    {label: "LOGO", href: "/"},
+    { label: "LOGO", href: "/" },
     { label: "Home", href: "/" },
     { label: "About Us", href: "/about" },
     { label: "Sermons", href: "/sermons" },
@@ -43,7 +50,7 @@ export function NavBar() {
   };
 
   return (
-    <nav className="flex flex-wrap  w-[520px] justify-around items-center  py-2 text-base font-semibold rounded-xl border border-solid bg-neutral-950 border-neutral-800 text-zinc-100 max-md:max-w-full">
+    <nav className="flex flex-wrap w-[520px] justify-around items-center py-2 text-base font-semibold rounded-xl border border-solid bg-neutral-950 border-neutral-800 text-zinc-100 max-md:max-w-full">
       {navItems.map((item, index) => (
         <NavItem
           key={index}
